79026e2579292742a32189f045674245a1c6562a
Shrika <shrikasomisetti@gmail.com>
2026-07-26 12:44:34 +0530

    Fix render report1

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def predict():
 
     return render_template("result.html", prediction=round(pred,2))
 if __name__=="__main__":
-    port = int(os.environment.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
